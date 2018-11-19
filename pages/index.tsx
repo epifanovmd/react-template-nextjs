7afadbcdb6a972e723e72457ce08ee7fd28b7e7c
Андрей Епифанов <epifanovmd@gmail.com>
2018-11-19 18:25:08 +0200

    Update README.md

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { AppContainer } from "../components/counter/container";
+import Head from 'next/head'
+
 class Index extends React.Component {
 
   render () {
     return (
         <div>
+            <Head>
+                <title>Next App</title>
+                <meta name="title" content={"Next App"} />
+                <meta name="description" content={"Next App"}  />
+            </Head>
             <AppContainer />
         </div>
     )
