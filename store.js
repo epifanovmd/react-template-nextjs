36e0948266f0fc83a8e0790e05923bae71ad0b0e
Epifanovmd <epifanovmd@gmail.com>
2019-08-15 14:29:58 +0300

    delete redux-logger

--- store.js
+++ store.js
@@ -1,14 +1,10 @@
 import {applyMiddleware, createStore} from "redux";
 import thunkMiddleware from "redux-thunk";
-import {logger} from "redux-logger";
 import {createMainReduce} from "./src/store/reducers";
 
 const exampleInitialState = {};
 
-const Middleware =
-  process.env.NODE_ENV === "development"
-    ? applyMiddleware(thunkMiddleware, logger)
-    : applyMiddleware(thunkMiddleware);
+const Middleware = applyMiddleware(thunkMiddleware);
 
 export function initializeStore(initialState = exampleInitialState) {
   const reducers = createMainReduce();
